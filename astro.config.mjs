7477dd7c835faca9de754417392da6a25717bfaf
longmeidao <longmeidao@pm.me>
2025-04-10 00:10:27 +0800

    更新 Astro 配置，添加图像服务支持，优化内容目录和远程图像模式配置，确保图像处理逻辑更为清晰。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { passthroughImageService } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
   // 你的 Astro 配置
   image: {
+    // service: passthroughImageService(),
     domains: [],
     remotePatterns: [
       {
@@ -14,7 +16,11 @@ export default defineConfig({
       }
     ],
     // 确保 Astro 能识别内容目录中的图片
-    contentDir: 'src/content'
+    contentDir: 'src/content',
+    service: {
+      entrypoint: passthroughImageService(),
+      config: {}
+    }
   },
   output: 'static',
   vite: {
